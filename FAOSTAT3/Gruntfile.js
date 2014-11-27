9b71880ca9ddf7aeb2be43a36196c5ff26ca2228
vortex <simone.murzilli@fao.org>
2014-11-27 15:58:51 +0100

    update

--- FAOSTAT3/Gruntfile.js
+++ FAOSTAT3/Gruntfile.js
@@ -6,7 +6,6 @@ grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-jsonlint');
 grunt.loadNpmTasks('grunt-contrib-watch');
@@ -31,43 +30,95 @@ grunt.initConfig({
 			src: ['dist/*']
 		}
 	},
-	jshint: {
-		options: {
-			globals: {
-				console: true,
-				module: true
-			},
-			"-W099": true,	//ignore tabs and space warning
-			"-W044": true,	//ignore regexp
-			"-W033": true
-		},
-		files: ['src/**/.js']
-	},
 	jsonlint: {
 		sample: {
 			src: [ 'i18n/*.json' ]
 		}
 	},
 	copy: {
-		fenixmapconfig: {
-			nonull: true,
-			src: 'src/FenixMapConfig.js',
-			dest: 'dist/fenix-ui-map-config.js'
-		},
-		imageslayers: {
-			nonull: true,
-			expand: true,
-			cwd: "src/css/images/",
-			src: '**',
-			dest: "dist/images/"
-		},
-		i18n: {
-			nonull: true,
-			expand: true,
-			cwd: 'src/i18n/',
-			src: '*',
-			dest: 'dist/i18n/'
-		}		
+        jquery: {
+            nonull: true,
+            src: 'node_modules/jquery/dist/jquery.min.js',
+            dest: 'src/lib/jquery.js'
+        },
+        backbone: {
+            nonull: true,
+            src: 'node_modules/backbone/backbone-min.js',
+            dest: 'src/lib/backbone.js'
+        },
+        "backbone.layoutmanager": {
+            nonull: true,
+            src: "node_modules/backbone.layoutmanager/backbone.layoutmanager.js",
+            dest: "src/lib/backbone.layoutmanager.js"
+        },
+        bootstrap_js: {
+            nonull: true,
+            src: "node_modules/bootstrap/dist/js/bootstrap.min.js",
+            dest: 'src/lib/bootstrap.js'
+        },
+        bootstrap_css: {
+            nonull: true,
+            src: "node_modules/bootstrap/dist/css/bootstrap.min.css",
+            dest: "src/lib/bootstrap.css"
+        },
+        nprogress_js: {
+            nonull: true,
+            src: "node_modules/nprogress/nprogress.js",
+            dest: "src/lib/nprogress.js"
+        },
+        nprogress_css: {
+            nonull: true,
+            src: "node_modules/nprogress/nprogress.css",
+            dest: "src/lib/nprogress.css"
+        },
+        requirejs: {
+            nonull: true,
+            src: "node_modules/requirejs/require.js",
+            dest: "src/lib/require.js"
+        },
+        domready: {
+            nonull: true,
+            src: "node_modules/domReady/domReady.js",
+            dest: "src/lib/domready.js"
+        },
+        text: {
+            nonull: true,
+            src: "node_modules/text/text.js",
+            dest: "src/lib/text.js"
+        },
+        i18n: {
+            nonull: true,
+            src: "node_modules/i18n/i18n.js",
+            dest: "src/lib/i18n.js"
+        },
+        underscore: {
+            nonull: true,
+            src: "node_modules/underscore/underscore-min.js",
+            dest: "src/lib/underscore.js"
+        },
+        jqwidgets_js: {
+            nonull: true,
+            src: "node_modules/jqwidgets/jqx-all.js",
+            dest: "src/lib/jqwidgets.js"
+        },
+        jqwidgets_css: {
+            nonull: true,
+            src: "node_modules/jqwidgets/styles/jqx.base.css",
+            dest: "src/lib/jqwidegts.css"
+        },
+        handlebars: {
+            nonull: true,
+            src: "node_modules/handlebars/dist/handlebars.min.js",
+            dest: "src/lib/handlebars.js"
+        },
+        jstree: {
+            nonull: true,
+            expand: true,
+            //flatten: true,
+            cwd: "node_modules/jstree/dist/",
+            src: '**',
+            dest: "src/lib/jstree/"
+        }
 	},
 	concat: {
 		options: {
@@ -77,7 +128,7 @@ grunt.initConfig({
 				block: true
 			}
 		},
-		lib: {
+		jquery_plugins: {
 			src: [
 				'node_modules/jquery/dist/jquery.min.js',
 				'node_modules/hoverintent/dist/hoverintent.min.js',
@@ -90,40 +141,27 @@ grunt.initConfig({
 				//TODO https://github.com/aaronsnoswell/csvjson.js
 				//TODO https://github.com/stefanocudini/csvjson.js
 				'lib/jquery.i18n.properties-min.js',
-				'lib/jquery-ui.custom.min.js',
+				'lib/jquery-ui.custom.min.js'
 			],
-			dest: 'dist/fenix-ui-map-lib.js'
+			dest: 'dist/fenix-ui-libs/jquery_plugins.min.js'
 		},
-		fenixmap: {
-			src: [
-				'src/FenixMap.js',
-				'src/core/Class.js',
-				'src/core/Util.js',
-				'src/core/HashMap.js',
-				//TODO move to lib
-				'src/core/UIUtils.js',
-				'src/core/WMSUtils.js',
-				'src/core/fullScreenApi.js',
-				//TODO move to lib
-				'src/map/config/*.js',
-				'src/map/Map.js',
-				'src/map/utils/LayerLegend.js',
-				'src/map/controller/MapControllerDraggable.js',
-				'src/map/utils/*.js ',
-				'src/map/layer/*.js'
-			],
-			dest: 'dist/fenix-ui-map.src.js'
-		}
-	},
-	uglify: {
-		options: {
-			banner: '<%= meta.banner %>'
-		},
-		fenixmap: {
-			files: {
-				'dist/fenix-ui-map.min.js': ['<%= concat.fenixmap.dest %>']
-			}
-		}
+        jquery_plugins_src: {
+            src: [
+                'node_modules/jquery/dist/jquery.min.js',
+                'node_modules/hoverintent/dist/hoverintent.min.js',
+                'node_modules/leaflet/dist/leaflet.js',
+                'node_modules/leaflet.markercluster/dist/leaflet.markercluster.js',
+                'node_modules/jquery-powertip/dist/jquery.powertip.min.js',
+                //TODO https://github.com/stevenbenner/jquery-powertip
+                //TODO https://github.com/stefanocudini/jquery-powertip
+                'node_modules/csvjson/csvjson.min.js',
+                //TODO https://github.com/aaronsnoswell/csvjson.js
+                //TODO https://github.com/stefanocudini/csvjson.js
+                'lib/jquery.i18n.properties-min.js',
+                'lib/jquery-ui.custom.min.js'
+            ],
+            dest: 'dist/fenix-ui-libs/jquery_plugins.js'
+        }
 	},
 	cssmin: {
 		options: {
@@ -141,23 +179,14 @@ grunt.initConfig({
 			cwd: 'dist/',
 			src: '<%= cssmin.combine.dest %>'
 		}
-	},
-	watch: {
-		dist: {
-			options: { livereload: true },
-			files: ['src/*'],
-			tasks: ['clean','concat','cssmin','jshint']
-		}		
 	}
 });
 
 grunt.registerTask('default', [
 	//'jshint',
 	'clean',
-	'concat:fenixmap',
-	'uglify',	
+	'concat:jquery_plugins',
 	'cssmin',
-	'jsonlint',	
 	'copy'
 ]);
 
